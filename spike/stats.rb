f2c8e7759e4e3be90e2eb69d96db30203d71afc1
Nigel Fernandes <dev@nigel.in>
2011-01-23 19:24:34 +0100

    got a basic spike with v8 going. Can convert raw JS source to a parse tree hash via JSLINT and V8 :-) :nigel

--- spike/stats.rb
+++ spike/stats.rb
@@ -1,23 +1,28 @@
+#require 'json'
+#
+#file          = 'places_parse.json'
+#file_contents = File.open(file, 'r') { |f| f.read }
+#tree          = JSON.parse(file_contents, :max_nesting => 100)
+#
+#puts tree[0]
+
+
+
+require '../boot.rb'
+require 'v8'
 require 'json'
 
+file = "places.js"
+source = File.open(file, 'r') { |f| f.read }
 
 
 
-file          = 'places_parse.json'
-file_contents = File.open(file, 'r') { |f| f.read }
-tree          = JSON.parse(file_contents, :max_nesting => 100)
+cxt = V8::Context.new
+cxt.load("json2.js")
+cxt.load("webjslint.js")
+cxt.load("options.js")
+cxt['source'] = source
+cxt.eval_js("JSLINT(source.toString(), options);")
+raw_json_tree = cxt.eval_js("JSON.stringify(JSLINT.tree, ['value',  'arity', 'name',  'first', 'second', 'third', 'block', 'else'], 4);")
 
-puts tree[0]
-
-
-
-
-  tree.each do |child|
-    child.each do |c|
-      yield c
-    end
-  end
-
-  yield self
-
-
+puts JSON.parse(raw_json_tree, :max_nesting => 100)
